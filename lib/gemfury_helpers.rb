4b5a7450caee0fb3b83f6364579479c6d5b3869a
Aaron Qian <aq1018@gmail.com>
2013-08-20 09:26:50 -0700

    add :account options to gem helper

--- lib/gemfury_helpers.rb
+++ lib/gemfury_helpers.rb
@@ -3,9 +3,26 @@ require "bundler"
 
 module GemfuryHelpers
   class GemHelper < Bundler::GemHelper
+    def self.install_tasks(opts={})
+      dir = opts[:dir] || Dir.pwd
+      self.new(dir, opts[:name], opts[:account]).install
+    end
+
+    attr_reader :account
+
+    def initialize(dir, name, account)
+      super(dir, name)
+      @account = account
+    end
+
     def rubygem_push(path)
-      sh("fury push '#{path}'")
+      sh push_cmd(path)
       Bundler.ui.confirm "Pushed #{name} #{version} to Gemfury"
+    end
+
+    def push_cmd(path)
+      cmd = "fury push '#{path}'"
+      account ? "#{cmd} --as #{account}" : cmd
     end
   end
 end
